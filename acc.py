ea824e0878d7851ac658ce04257143eb40febf60
D.M. Cook <dmaterialized@gmail.com>
2017-06-27 00:54:29 -0400

    create basic methods

--- acc.py
+++ acc.py
@@ -1,11 +1,28 @@
 # a very simple program to demonstrate OOP concepts
 
-class account:
-     def __init__(self,filepath):
+class Account:
+    def __init__(self,filepath):
      # first read the number from the balance file
-     with open(filepath,'r') as file:
-         self.balance=int(file.read())
-        # read the file and store value in "self.balance"
+        with open(filepath,'r') as file:
+            self.balance=int(file.read())
+            # read the file and store value in "self.balance"
+
+    def withdraw(self, amount):
+        # calculate subtraction
+        self.balance=self.balance - amount
+        # don't use "with open('file')" when you can make a method for commit
+
+    def deposit(self, amount):
+        # calculate addition
+        self.balance=self.balance + amount
+
 
 # create vars to store account
-account=Account("balance.txt")
+account=Account("accounts/balance.txt") #var is the result of a class
+# for some reason we must declare enclosing folder here or it won't work.
+print(account.balance) #dot notation points to the attribute of the object 'account'.
+
+# ========= DEV OPS ============
+account.withdraw(100)
+print(account.balance)
+# works.
