82075e73fdf9c844a0519086843a76020cceec48
D.M. Cook <dmaterialized@gmail.com>
2017-06-27 01:03:26 -0400

    create commit method

--- acc.py
+++ acc.py
@@ -2,6 +2,7 @@
 
 class Account:
     def __init__(self,filepath):
+        self.filepath=filepath # instance variable - see line 21
      # first read the number from the balance file
         with open(filepath,'r') as file:
             self.balance=int(file.read())
@@ -16,7 +17,15 @@ class Account:
         # calculate addition
         self.balance=self.balance + amount
 
-
+    def commit(self):
+        # with open(...) # can't use filepath as that's only in the init method!
+        # one way would be
+        # def commit(self,path):
+        # another way
+        # let's do a "instance variable" instead, in the init, which stores in Self..
+        # self.filepath=filepath is defined in init (line 5)
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
 # create vars to store account
 account=Account("accounts/balance.txt") #var is the result of a class
 # for some reason we must declare enclosing folder here or it won't work.
@@ -25,4 +34,4 @@ print(account.balance) #dot notation points to the attribute of the object 'acco
 # ========= DEV OPS ============
 account.withdraw(100)
 print(account.balance)
-# works.
+account.commit() # pass this to commit data.
